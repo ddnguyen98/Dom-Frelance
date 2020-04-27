c4332d60d6e9faae4d45dc29ec8ea7cdbf15189e
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2020-04-27 16:15:06 -0700

    Updates

---
+++ precache-manifest.c8371bc7ba0339a88b34736308872b30.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dfa03c21cb30708904d762c7dd0eba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e880717349bf6efab922",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5437ffea16a1c7579d71",
+    "url": "/static/css/main.bca96873.chunk.css"
+  },
+  {
+    "revision": "e880717349bf6efab922",
+    "url": "/static/js/2.1b22c7fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1b22c7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5437ffea16a1c7579d71",
+    "url": "/static/js/main.f6d86490.chunk.js"
+  },
+  {
+    "revision": "bd918ea78b12e9028c59",
+    "url": "/static/js/runtime-main.b2eba50f.js"
+  },
+  {
+    "revision": "86f1dccc303b101af960a29525807466",
+    "url": "/static/media/bike.86f1dccc.png"
+  },
+  {
+    "revision": "5f90ab42bb49a115b2b3391ce2cbbe9b",
+    "url": "/static/media/carol.5f90ab42.png"
+  },
+  {
+    "revision": "1ab981b4076d2e5d19b32898fc5fd233",
+    "url": "/static/media/celebrity.1ab981b4.png"
+  },
+  {
+    "revision": "b11c49530cfe7ecb4f45a614ca00066d",
+    "url": "/static/media/dogily.b11c4953.jpg"
+  },
+  {
+    "revision": "9be3c5630b99e9d101ee4bcd14fcd0e1",
+    "url": "/static/media/head.9be3c563.jpg"
+  },
+  {
+    "revision": "5d3012f3fc400f1457a5f674a34defff",
+    "url": "/static/media/me.5d3012f3.jpg"
+  },
+  {
+    "revision": "496ba36b932e3acdcfb0264a9570cf7f",
+    "url": "/static/media/paw.496ba36b.png"
+  },
+  {
+    "revision": "9149f9d249f79179b384ba1a9ae0312f",
+    "url": "/static/media/star.9149f9d2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Dom-Frelance/precache-manifest.bb688ecab76909a395065c5eadf6a4c6.js"
+  "/precache-manifest.c8371bc7ba0339a88b34736308872b30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dom-Frelance/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
