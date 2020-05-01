ac16d85a6cd801acabea47f2b130a56b1415f988
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2020-05-01 14:04:18 -0700

    Stylizing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
   Container
 } from 'reactstrap';
 
-import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin, FaExternalLinkAlt } from "react-icons/fa";
 import logo from './star.svg';
 import me from './me.jpg';
 import carol from './img/carol.png';
@@ -39,6 +39,8 @@ function App() {
       suffix: "%",
       gridColor: "white",
       labelFontSize: 0,
+      tickThickness: 0,
+      lineThickness: 0,
       fontFamily: 'Roboto',
     },
     data: [{				
@@ -146,7 +148,7 @@ function App() {
               <h3>WordPress</h3>
               <p>Brainstormed brand and site identity with client and made sure their preferences were incorporated. I planned and designed the site, gathered assets and information for the layout, and finished the entire process in less than a month. Im currently the web master of this site providing technical assistance when needed.
               </p>
-              <p><a href="https://lcarolholt.com/" target="_blank" rel="noopener noreferrer">Site Link</a></p>
+              <a href="https://lcarolholt.com/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
             </div>
           </div>
           <div className="articles">
@@ -156,7 +158,7 @@ function App() {
               <h3>React/Redux</h3>
               <p>This site was my final submission for Full Sail University and it displays my ability to handle large amounts of data and to keep track and store them in the database. Each item, weapon, skill, feat, and spell is tracked by a character ID, which is tied to a user ID. The site functions as a character creator for dnd 3.5e, and it allows users to keep track of their characters they can store for themselves. This site was difficult to construct, because of the asynchronous nature of javascript. I really enjoy interacting with the back end of a site and hope this shows off my skills as a developer.
               </p>
-              <p><a href="https://github.com/fs-tech-degs/capstone-project-1911-ddnguyen98" target="_blank" rel="noopener noreferrer">Github Link</a></p>
+              <a href="https://github.com/fs-tech-degs/capstone-project-1911-ddnguyen98" target="_blank" rel="noopener noreferrer"><button className="abutton">Github <FaExternalLinkAlt /></button></a>
             </div>
           </div>
           <div className="articles">
@@ -165,8 +167,8 @@ function App() {
               <h2>Bike shop</h2>
               <h3>Javascript</h3>
               <p>When designing this site from scratch, I practiced proper html semantic code and used pre-given assets. This project also included preplanning and setting up asset boards.</p>
-              <p><a href="https://github.com/ddnguyen98/ddnguyen98.github.io/tree/master" target="_blank" rel="noopener noreferrer">Github Link</a>
-              <a href="https://ddnguyen98.github.io/BikeShop/" target="_blank" rel="noopener noreferrer">Site Link</a></p>
+              <a href="https://github.com/ddnguyen98/ddnguyen98.github.io/tree/master" target="_blank" rel="noopener noreferrer"><button className="abutton">Github <FaExternalLinkAlt /></button></a>
+              <a href="https://ddnguyen98.github.io/BikeShop/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
             </div>
           </div>
           <div className="articles">
@@ -175,8 +177,8 @@ function App() {
               <h2>Paw Pals</h2>
               <h3>React/Firebase</h3>
               <p>Paw pals is a site where dog owners arrange play dates with other dog owners that sign up for the site. Using data created from a random user API along with a dog api that spit out a dog based on the first letter of a user's name, a profile was created with those dog details. The list was saved to a matches page where users can view dates declined and accepted. I used Google Firebase to set up user authentication that stored user json data.</p>
-              <p><a href="https://github.com/ddnguyen98/deployPP5" target="_blank" rel="noopener noreferrer">Github Link</a>
-              <a href="https://ddnguyen98.github.io/deployPP5/#/" target="_blank" rel="noopener noreferrer">Site Link</a></p>
+              <a href="https://github.com/ddnguyen98/deployPP5" target="_blank" rel="noopener noreferrer"><button className="abutton">Github <FaExternalLinkAlt /></button></a>
+              <a href="https://ddnguyen98.github.io/deployPP5/#/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
             </div>
           </div>
           <div className="articles">
@@ -185,8 +187,8 @@ function App() {
               <h2>Dogily</h2>
               <h3>Javascript</h3>
               <p>Dogily is a concept site where users can adopt dogs based on an opening quiz. This project was a test of my designing skills, and I practiced my use of css and design principles in this project.</p>
-              <p><a href="https://github.com/ddnguyen98/Dogily" target="_blank" rel="noopener noreferrer">Github Link</a>
-              <a href="https://ddnguyen98.github.io/Dogily/html/" target="_blank" rel="noopener noreferrer">Site Link</a></p>
+              <a href="https://github.com/ddnguyen98/Dogily" target="_blank" rel="noopener noreferrer"><button className="abutton">Github <FaExternalLinkAlt /></button></a>
+              <a href="https://ddnguyen98.github.io/Dogily/html/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
             </div>
           </div>
         </Container>
