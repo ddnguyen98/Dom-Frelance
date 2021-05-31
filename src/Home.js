7013e4fe578628a2db69c6ba831beaf2b1cdec6d
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2021-05-31 14:00:04 -0700

    Grammar edits and css tweaks.

--- src/Home.js
+++ src/Home.js
@@ -99,7 +99,6 @@ function App() {
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
-
         <Navbar dark expand="md" className="navigation">
           <Container> 
               <NavbarToggler onClick={toggle} />
@@ -124,20 +123,14 @@ function App() {
               </Collapse>
           </Container>
         </Navbar>
-        
         <div id="about" className="zindex">
           <Container >
             <h1>About Me</h1>
             <div className="aboutcontent">
               <div className="aboutcontent2">
                 <img src={me} alt="Dominc Nguyen" width='250px' height='300px'></img>
-                <p>I am a full stack developer with a passion for back end 
-                and database creation. Designing website's was not my first thought for a career path but I found
-                that both my creative and technical side to my brain made me gravitate to the world of web development. I actually created a website
-                for an english project about Cthulhu since I didn't want to just make another boring essay. 
-                You can view the site <a href="https://shamze33.wixsite.com/cthulhuslibrary" className="highlight">here</a> to get a view of my first
-                ever site I made in highschool. Take a look around the site to get to know my style of website development and contact me with any
-                opportunities.</p>
+                <p>I am a full stack developer with a passion for back end and database creation, because designing web sites engages both my creative and technical sides. My career path actually started in high school, when I opted to create a web site for an English project because I didn’t want to turn in just another boring essay. 
+                You can view the site <a href="https://shamze33.wixsite.com/cthulhuslibrary" className="highlight">here</a> that I made for my english project. For some examples of more current work, and to contact me to work on your project, just scroll around my site.</p>
               </div>  
               <CanvasJSChart options = {options}/>
             </div>
@@ -146,7 +139,7 @@ function App() {
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
-        <div id="client">
+        <div id="client" className="zindex">
           <Container>
           <h1>Client Sites</h1>
             <div className="articles">
@@ -154,7 +147,7 @@ function App() {
               <div className="articlesContent">
                 <h2>L Carol Holt Associates</h2>
                 <h3>WordPress</h3>
-                <p>Brainstormed brand and site identity with client and made sure their preferences were incorporated. I planned and designed the site, gathered assets and information for the layout, and finished the entire process in less than a month. Im currently the web master of this site providing technical assistance when needed.
+                <p>Brainstormed brand and site identity with client and made sure their preferences were incorporated. I planned and designed the site, gathered assets and information for the layout, and finished the entire process in less than a month. I'm currently the web master of this site, providing technical assistance when needed.
                 </p>
                 <a href="https://lcarolholt.com/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
               </div>
@@ -164,10 +157,10 @@ function App() {
               <div className="articlesContent">
                 <h2>AND? Inventive Negotiation</h2>
                 <h3>PHP + HTML</h3>
-                <p>Updated old css and html to meet with the new design of book for my client. Made sure keep in constant contact with client as to not leave them out of the creation progress and to keep them informed to anything they disliked. Created a new logo from scratch for clients new site as well as some assets. 
+                <p>Updated old css and html to meet with the new design of the book for my client. Kept in constant contact with the client to insure that he was part of the creative process and to respond to his likes and dislikes. I created a new logo from scratch and added new assets on this site.
                 </p>
                 <a href="http://www.inventivenegotiation.com/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
-              </div>
+              </div>    
             </div>
           </Container>
         </div>
@@ -202,7 +195,7 @@ function App() {
               <div className="articlesContent">
                 <h2>Paw Pals</h2>
                 <h3>React/Firebase</h3>
-                <p>Paw pals is a site where dog owners arrange play dates with other dog owners that sign up for the site. Using data created from a random user API along with a dog api that spit out a dog based on the first letter of a user's name, a profile was created with those dog details. The list was saved to a matches page where users can view dates declined and accepted. I used Google Firebase to set up user authentication that stored user json data.</p>
+                <p>Paw pals is a site where dog owners arrange play dates with other dog owners that sign up for the site. Using data created from a random user API along with a dog API that spit out a dog based on the first letter of a user's name, a profile was created with those dog details. The list was saved to a matches page where users can view dates declined and accepted. I used Google Firebase to set up user authentication that stored user json data.</p>
                 <a href="https://github.com/ddnguyen98/deployPP5" target="_blank" rel="noopener noreferrer"><button className="abutton">Github <FaExternalLinkAlt /></button></a>
                 <a href="https://ddnguyen98.github.io/deployPP5/#/" target="_blank" rel="noopener noreferrer"><button className="abutton">Site <FaExternalLinkAlt /></button></a>
               </div>
@@ -225,7 +218,7 @@ function App() {
             <h1>Testimonials</h1>
             <div className="testimonialcontent">
               <h3>"I have no idea how I could work remotely without Dominic's skills."</h3>
-              <p>- Tomas Salinger</p>
+              <p>- Thomas Salinger</p>
             </div>
             <div className="testimonialcontent">
               <h3>"Dominic’s combination of technical and motivational expertise made him the perfect partner to develop my web-site. His energy is positive and earnest and inspired me! "</h3>
